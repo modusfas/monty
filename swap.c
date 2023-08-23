58182b3bb02dd50822d9e8c9a28adfdd3b6fb50f
modusfas <modupefasunon@gmail.com>
2023-08-23 12:43:42 -0700

    swap errors

---
+++ swap.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * f_swap - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: nothing
+*/
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	
+	int temp = (*stack)->n;
+	
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
