2674a32ba5102edb7d4fb3a69e54e8fec5034079
modusfas <modupefasunon@gmail.com>
2023-08-23 12:21:32 -0700

    pop upcode

---
+++ push.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * push - push  stack value
+ * @stack: pointer to a pointer
+ * @value: number
+ *
+ * Return: zero on success
+ */
+void push(stack_t **stack, int value)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+}
